be0694dcdd4df819dce67b6bcf963d287ac3b79c
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-05-20 20:41:43 +0300

    feat: add HelloController for user, cart, order, and product services

---
+++ order-service/src/main/java/com/turkcell/order_service/HelloController.java
@@ -0,0 +1,13 @@
+package com.turkcell.order_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Order Service";
+	}
+}
---
+++ product-service/src/main/java/com/turkcell/product_service/HelloController.java
@@ -0,0 +1,13 @@
+package com.turkcell.product_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Product Service";
+	}
+}
